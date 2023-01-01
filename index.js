c7df984e96932459a40ad8167cac78022bc4bfaf
Nicolas Sapountzis <nsap95@gmail.com>
2023-01-01 11:38:08 +0700

    commit tree 05983b8104b7268f1156ef4c8c4c4f4a313f81b6

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a-b
+}
+
+function multiply(a,b) {
+    return a*b
+}
+
+function divide(a,b) {
+    return a/b
+}
+
+function increment(n) {
+    return n+=1
+}
+
+function decrement(n) {
+    return n-=1
+}
+
+
+let c = "709"
+
+function makeInt(c) {
+   return parseInt(c, 10);
+}
+
+let d = "2.222"
+function preserveDecimal(d) {
+    return parseFloat(d);
+}
